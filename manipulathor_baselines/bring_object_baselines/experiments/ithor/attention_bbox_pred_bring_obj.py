87072fb01d376ae03117cdb2e5c7093c18a6f3a4
Kiana Ehsani <ehsanik@gmail.com>
2021-06-29 13:50:20 -0700

    testing lucas changes, adding centerpoint detection

---
+++ manipulathor_baselines/bring_object_baselines/experiments/ithor/attention_bbox_pred_bring_obj.py
@@ -0,0 +1,127 @@
+import platform
+
+import gym
+from allenact.algorithms.onpolicy_sync.losses import PPO
+from allenact.algorithms.onpolicy_sync.losses.ppo import PPOConfig
+from allenact.utils.experiment_utils import TrainingPipeline, Builder, PipelineStage, LinearDecay
+from allenact_plugins.ithor_plugin.ithor_sensors import RGBSensorThor
+from torch import nn, optim
+from torch.optim.lr_scheduler import LambdaLR
+
+from ithor_arm.bring_object_sensors import TargetObjectBBox, TargetLocationBBox
+from ithor_arm.bring_object_task_samplers import DiverseBringObjectTaskSampler, OnlyAppleBreadBringObjectTaskSampler
+from ithor_arm.ithor_arm_constants import ENV_ARGS, TRAIN_OBJECTS, TEST_OBJECTS
+from ithor_arm.ithor_arm_sensors import (
+    InitialAgentArmToObjectSensor,
+    InitialObjectToGoalSensor,
+    PickedUpObjSensor,
+    DepthSensorThor, RelativeAgentArmToObjectSensor, RelativeObjectToGoalSensor,
+)
+from manipulathor_baselines.bring_object_baselines.experiments.bring_object_mixin_ddppo import BringObjectMixInPPOConfig
+from manipulathor_baselines.bring_object_baselines.experiments.bring_object_mixin_simplegru import BringObjectMixInSimpleGRUConfig
+from manipulathor_baselines.bring_object_baselines.experiments.ithor.bring_object_ithor_base import BringObjectiThorBaseConfig
+from manipulathor_baselines.bring_object_baselines.losses.bring_object_losses import PredictBoxBCELsss
+from manipulathor_baselines.bring_object_baselines.models.pickup_object_with_mask_model import PickUpWMaskBaselineActorCritic
+from manipulathor_baselines.bring_object_baselines.models.pred_box_bring_object import PredictBBoxwTFBringObjectModel
+from manipulathor_baselines.bring_object_baselines.models.small_bring_object_with_mask_model import SmallBringObjectWMaskDepthBaselineActorCritic
+from manipulathor_baselines.bring_object_baselines.models.small_depth_pickup_object_with_mask_model import SmallPickUpWMaskDepthBaselineActorCritic
+
+
+class AttentionBBoxBringObject(
+    BringObjectiThorBaseConfig,
+    BringObjectMixInPPOConfig,
+    BringObjectMixInSimpleGRUConfig,
+):
+    """An Object Navigation experiment configuration in iThor with RGB
+    input."""
+
+    SENSORS = [
+        DepthSensorThor(
+            height=BringObjectiThorBaseConfig.SCREEN_SIZE,
+            width=BringObjectiThorBaseConfig.SCREEN_SIZE,
+            use_normalization=True,
+            uuid="depth_lowres",
+        ),
+        RGBSensorThor(
+            height=BringObjectiThorBaseConfig.SCREEN_SIZE,
+            width=BringObjectiThorBaseConfig.SCREEN_SIZE,
+            use_resnet_normalization=True,
+            uuid="rgb_lowres",
+        ),
+        PickedUpObjSensor(),
+        TargetObjectBBox(),
+        TargetLocationBBox(),
+    ]
+
+    MAX_STEPS = 200
+    TASK_SAMPLER = OnlyAppleBreadBringObjectTaskSampler
+    NUM_PROCESSES = 40
+    # TRAIN_SCENES = ['FloorPlan1_physics']
+    # TEST_SCENES = ['FloorPlan1_physics']
+    OBJECT_TYPES = TRAIN_OBJECTS + TEST_OBJECTS
+
+    def __init__(self):
+        super().__init__()
+
+        assert (
+            self.CAMERA_WIDTH == 224
+            and self.CAMERA_HEIGHT == 224
+            and self.VISIBILITY_DISTANCE == 1
+            and self.STEP_SIZE == 0.25
+        )
+        self.ENV_ARGS = {**ENV_ARGS, "renderDepthImage": True}
+
+    @classmethod
+    def create_model(cls, **kwargs) -> nn.Module:
+        return PredictBBoxwTFBringObjectModel(
+            action_space=gym.spaces.Discrete(
+                len(cls.TASK_SAMPLER._TASK_TYPE.class_action_names())
+            ),
+            observation_space=kwargs["sensor_preprocessor_graph"].observation_spaces,
+            hidden_size=512,
+        )
+
+    def training_pipeline(self, **kwargs):
+        ppo_steps = int(300000000)
+        lr = 3e-4
+        num_mini_batch = 1
+        update_repeats = 4
+        num_steps = 128 #self.MAX_STEPS
+        #TODO try with smaller
+        #TODO remove
+        num_steps = 3
+        save_interval = 500000  # from 50k
+        log_interval = 1000
+        gamma = 0.99
+        use_gae = True
+        gae_lambda = 0.95
+        max_grad_norm = 0.5
+        return TrainingPipeline(
+            save_interval=save_interval,
+            metric_accumulate_interval=log_interval,
+            optimizer_builder=Builder(optim.Adam, dict(lr=lr)),
+            num_mini_batch=num_mini_batch,
+            update_repeats=update_repeats,
+            max_grad_norm=max_grad_norm,
+            num_steps=num_steps,
+            named_losses={"ppo_loss": PPO(**PPOConfig), "pred_box_bce": PredictBoxBCELsss()},
+            gamma=gamma,
+            use_gae=use_gae,
+            gae_lambda=gae_lambda,
+            advance_scene_rollout_period=self.ADVANCE_SCENE_ROLLOUT_PERIOD,
+            pipeline_stages=[
+                # PipelineStage(loss_names=["ppo_loss"], max_stage_steps=ppo_steps)
+                PipelineStage(
+                    loss_names=["ppo_loss", "pred_box_bce"],
+                    loss_weights=[1.0, 1.0], #TODO how is this?
+                    max_stage_steps=ppo_steps,
+                )
+            ],
+
+            lr_scheduler_builder=Builder(
+                LambdaLR, {"lr_lambda": LinearDecay(steps=ppo_steps)}
+            ),
+        )
+    @classmethod
+    def tag(cls):
+        return cls.__name__
---
+++ manipulathor_baselines/bring_object_baselines/losses/bring_object_losses.py
@@ -0,0 +1,72 @@
+"""Defining imitation losses for actor critic type models."""
+
+from typing import Dict, cast
+
+import torch
+
+from allenact.algorithms.onpolicy_sync.losses.abstract_loss import (
+    AbstractActorCriticLoss,
+    ObservationType,
+)
+from allenact.base_abstractions.distributions import CategoricalDistr
+from allenact.base_abstractions.misc import ActorCriticOutput
+
+from manipulathor_utils.debugger_util import ForkedPdb
+
+
+class PredictBoxBCELsss(AbstractActorCriticLoss):
+    """Expert imitation loss."""
+
+    def loss(  # type: ignore
+            self,
+            step_count: int,
+            batch: ObservationType,
+            actor_critic_output: ActorCriticOutput[CategoricalDistr],
+            *args,
+            **kwargs
+    ):
+        """Computes the Prediction Box loss.
+
+        # Parameters
+
+        batch : A batch of data corresponding to the information collected when rolling out (possibly many) agents
+            over a fixed number of steps. In particular this batch should have the same format as that returned by
+            `RolloutStorage.recurrent_generator`.
+            Here `batch["observations"]` must contain `"expert_action"` observations
+            or `"expert_policy"` observations. See `ExpertActionSensor` (or `ExpertPolicySensor`) for an example of
+            a sensor producing such observations.
+        actor_critic_output : The output of calling an ActorCriticModel on the observations in `batch`.
+        args : Extra args. Ignored.
+        kwargs : Extra kwargs. Ignored.
+
+        # Returns
+
+        A (0-dimensional) torch.FloatTensor corresponding to the computed loss. `.backward()` will be called on this
+        tensor in order to compute a gradient update to the ActorCriticModel's parameters.
+        """
+
+        ForkedPdb().set_trace()
+
+        observations = cast(Dict[str, torch.Tensor], batch["observations"])
+        extra_model_outputs = actor_critic_output.extras
+
+        gt_relative_agent_arm_to_obj = observations['relative_agent_arm_to_obj']
+        gt_relative_obj_to_goal = observations['relative_obj_to_goal']
+
+        pred_agent_arm_to_obj = extra_model_outputs['relative_agent_arm_to_obj_prediction']
+        pred_obj_to_goal = extra_model_outputs['relative_agent_obj_to_goal_prediction']
+
+        assert gt_relative_agent_arm_to_obj.shape == pred_agent_arm_to_obj.shape
+        assert gt_relative_obj_to_goal.shape == pred_obj_to_goal.shape
+        ForkedPdb().set_trace()
+        loss_function = torch.nn.SmoothL1Loss() #LATER_TODO is this a good choice?
+        arm_to_obj_loss = loss_function(gt_relative_agent_arm_to_obj, pred_agent_arm_to_obj)
+        obj_to_goal_loss = loss_function(gt_relative_obj_to_goal, pred_obj_to_goal)
+        total_loss = arm_to_obj_loss + obj_to_goal_loss
+
+
+
+        return (
+            total_loss,
+            {"pred_box_bce": total_loss.item(),}
+        )
