e5cc8794fe5724394cf795d4ebbd61abe2f7034b
Karl Schmeckpeper <karls@allenai.org>
2022-08-01 15:29:32 -0700

    Fixed typo

--- manipulathor_baselines/stretch_bring_object_baselines/experiments/ithor/pointnav_emul_stretch_all_rooms_noisy_motion_with_isdf_distrib.py
+++ manipulathor_baselines/stretch_bring_object_baselines/experiments/ithor/pointnav_emul_stretch_all_rooms_noisy_motion_with_isdf_distrib.py
@@ -1,6 +1,6 @@
 import torch
 
-from manipulathor_baselines.stretch_bring_object_baselines.experiments.ithor.pointnav_emul_stretch_all_rooms_with_isdf \
+from manipulathor_baselines.stretch_bring_object_baselines.experiments.ithor.pointnav_emul_stretch_all_rooms_noisy_motion_with_isdf \
     import PointNavEmulStretchAllRoomsNoisyMotionWithISDF
 
 
