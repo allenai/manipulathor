8a631ac4f8d27eb75b341e70acc8e0be35a3a10b
Karl Schmeckpeper <karls@allenai.org>
2022-08-03 09:48:09 -0700

    REduced num processes

--- manipulathor_baselines/stretch_bring_object_baselines/experiments/ithor/pointnav_emul_stretch_all_rooms_noisy_motion_with_map_distrib.py
+++ manipulathor_baselines/stretch_bring_object_baselines/experiments/ithor/pointnav_emul_stretch_all_rooms_noisy_motion_with_map_distrib.py
@@ -7,7 +7,7 @@ from manipulathor_baselines.stretch_bring_object_baselines.experiments.ithor.poi
 class PointNavEmulStretchAllRoomsNoisyMotionWithMapDistrib(
     PointNavEmulStretchAllRoomsNoisyMotionWithMap
 ):
-    NUM_PROCESSES = 15
+    NUM_PROCESSES = 11
     def __init__(
             self,
             distributed_nodes: int = 4,
